72271eacccff57383bd195970b810076ce0df63a
Goutemberg Pessoa <goutemberg@icloud.com>
2021-03-10 21:37:33 -0300

    Finalizacao dos cenario de login e cadastro

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,7 @@
+Dado('que acesso a página principal') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('submeto minhas credenciais {string} e {string}') do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
